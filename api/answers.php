de69f3cce433c873b1ef63f825a707973fe95622
Justin Vos <justinvosnz@gmail.com>
2016-02-16 15:42:21 +1300

    Moved both GET and POST methods from answer.php to answers.php.

--- api/answers.php
+++ api/answers.php
@@ -7,41 +7,7 @@
     'error_msg' => ''
   );
 
-  if($_SERVER['REQUEST_METHOD'] == 'GET')
-  {
-    if(isset($_REQUEST['question_id']))
-    {
-      $db = connect();
-
-      $query = $db->prepare("SELECT answers.id,answers.question,answers.answer,COUNT(votes.id) AS `count` FROM answers LEFT JOIN votes ON answers.id=votes.answer WHERE answers.question=:question_id GROUP BY answers.id;");
-
-      $query->bindParam(":question_id", $question_id);
-      $question_id = $_REQUEST['question_id'];
-
-      $query->execute();
-      $dataset = $query->fetchAll();
-
-      $results['answers'] = array();
-
-      for($i = 0; $i < sizeof($dataset); $i++)
-      {
-        array_push($results['answers'], array(
-          'id' => $dataset[$i]['id'],
-          'question' => $dataset[$i]['question'],
-          'answer' => $dataset[$i]['answer'],
-          'count' => $dataset[$i]['count']
-        ));
-      }
-    }
-    else
-    {
-      http_response_code(400);
-
-      $results['error'] = true;
-      $results['error_msg'] = 'Missing paramater(s)';
-    }
-  }
-  else if($_SERVER['REQUEST_METHOD'] == 'POST')
+  if($_SERVER['REQUEST_METHOD'] == 'POST')
   {
     if(isset($_REQUEST['account']) && isset($_REQUEST['token']) && isset($_REQUEST['question_id']) && isset($_REQUEST['answers']))
     {
