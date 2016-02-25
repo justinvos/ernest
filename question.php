359c956f02fffbd2d3908bff16ef3e23a242ae23
Justin Vos <justinvosnz@gmail.com>
2016-02-25 15:53:30 +1300

    Removed all dependencies on frontend.php.

--- question.php
+++ question.php
@@ -1,6 +1,20 @@
 <?php
 
-  include('frontend.php');
+  session_start();
+
+  if(!isset($_SESSION['account']) || !isset($_SESSION['token']))
+  {
+    header('Location: error.php?type=nosession');
+  }
+
+  $curl = curl_init("localhost/ernest/api/session.php?account=" . $_SESSION['account'] . "&token=" . $_SESSION['token']);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $session = json_decode(curl_exec($curl), true);
+
+  if(!isset($session) || sizeof($session) == 0 || !$session['authenticated'])
+  {
+    header('Location: error.php?type=nosession');
+  }
 
   if(!isset($_REQUEST['id']))
   {
